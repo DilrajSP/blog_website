27505f49d39b35014c06b079e2841f64cfe2c559
DilrajSP <dil476raj@gmail.com>
2024-08-08 21:55:06 +0530

    Page scroll position not reset to top on navigation solved

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,16 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import './App.css'
 import { Signin } from './pages/Signin'
 import { Signup } from './pages/Signup'
 import Blog from './pages/Blog'
 import { Blogs } from './pages/Blogs'
 import { Publish } from './components/Publish'
+import { useEffect } from 'react'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path='/signup' element={<Signup />} />
           <Route path='/signin' element={<Signin />} />
@@ -21,6 +23,14 @@ function App() {
       </BrowserRouter>
     </>
   )
+}
+
+function ScrollToTop() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  return null; // Render nothing
 }
 
 export default App
