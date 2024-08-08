6b2c5c0dadfde6a155681ea862b6204ab3c91a67
DilrajSP <dil476raj@gmail.com>
2024-08-08 22:23:27 +0530

    minor fixes

--- Frontend/src/pages/Blogs.tsx
+++ Frontend/src/pages/Blogs.tsx
@@ -11,7 +11,7 @@ export const Blogs = () => {
         return <div>
             <AppBar />
             <div className="flex justify-center">
-                <div>
+                <div className="mt-20">
                     <BlogsSkeleton />
                     <BlogsSkeleton />
                     <BlogsSkeleton />
